7ab31e23bc46a9af6a26feaa4d21e22ed0ee5684
tamura.naoki <tamu.0.0.tamu@gmail.com>
2018-11-09 08:52:44 +0900

    Update docker install script.

--- my_dev_vagrant/scripts/base/docker/install.sh
+++ my_dev_vagrant/scripts/base/docker/install.sh
@@ -6,7 +6,7 @@ sudo apt -y install apt-transport-https ca-certificates curl software-properties
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
 sudo apt update
 
