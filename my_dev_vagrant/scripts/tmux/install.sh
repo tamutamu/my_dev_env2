d3f2ac221f51d5ecad26262477766f704a41c664
tamura.naoki <tamu.0.0.tamu@gmail.com>
2018-11-09 12:06:34 +0900

    Add xsel for tmux-yank.

--- my_dev_vagrant/scripts/tmux/install.sh
+++ my_dev_vagrant/scripts/tmux/install.sh
@@ -7,7 +7,7 @@ cd ${CURDIR}
 
 
 ### Install tmux
-sudo apt -y install tmux
+sudo apt -y install tmux xsel
 
 ln -s ${CURDIR}/../.dotfiles/.tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
