7a37e00bce1cc4530bc6e791b33aab7565daafe3
tamutamu <tamu.0.0.tamu@gmail.com>
2019-01-04 10:13:16 +0900

    Update nodejs.sh

--- my_dev_vagrant/scripts/base/js/nodejs.sh
+++ my_dev_vagrant/scripts/base/js/nodejs.sh
@@ -8,3 +8,7 @@ sudo npm -g install n
 sudo n stable
 sudo ln -sf /usr/local/bin/node /usr/bin/node
 
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.profile
+source ~/.profile
