8e086386bd129174bd9d9114dd00e13f515c86a1
tamutamu <tamu.0.0.tamu@gmail.com>
2019-01-10 13:19:45 +0900

    更新

--- my_dev_vagrant/base_provision.bat
+++ my_dev_vagrant/base_provision.bat
@@ -1,6 +1,6 @@
 vagrant destroy -f
 vagrant up --provision-with kernel
 vagrant reload
-vagrant up --provision-with base
+vagrant up --provision-with my_dev_base
 vagrant halt
 vagrant snapshot save --force base
--- my_dev_vagrant/scripts/base/kernel/install.sh
+++ my_dev_vagrant/scripts/base/kernel/install.sh
@@ -2,5 +2,5 @@
 
 sed -i.bak -e "s%http://archive.ubuntu.com/ubuntu%http://ftp.iij.ad.jp/pub/linux/ubuntu/archive/%g" /etc/apt/sources.list
 apt update
-apt -y upgrade
-apt -y --force-yes install linux-headers-generic
+apt upgrade -y
+apt install -y --force-yes  linux-headers-generic
